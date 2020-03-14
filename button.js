aee9732d08c1f7fb154f9c5b34d1b87f57e70111
simyeen <60914410+simyeen@users.noreply.github.com>
2020-03-14 15:41:58 +0900

    Add files via upload

---
+++ button.js
@@ -0,0 +1,18 @@
+<body>
+
+
+<script>
+  window.onload = function() {
+
+  var hm = document.getElementById('hm');
+
+  hm.addEvenListener('click',function() {
+
+    alert('Hello world');
+
+  })
+
+  }
+</script>
+
+</body>
